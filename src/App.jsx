fc4577f5207b201b68e422f5e2e0ee998997bc03
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-06-09 19:04:51 -0400

    react-router-dom dependency added

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,38 @@ import { useState } from 'react';
 function App() {
   const [data, setData] = useState('initial data');
 
-  useEffect(() => {
-    console.log('useEffect');
-    async function fetchData() {
-      const response = await fetch('data.json');
-      const data = await response.json();
-      setData(data.data[0].job_name);
-      return data;
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   console.log('useEffect');
+  //   async function fetchData() {
+  //     const response = await fetch('data.json');
+  //     const data = await response.json();
+  //     setData(data.data[0].job_name);
+  //     return data;
+  //   }
+  //   fetchData();
+  // }, []);
 
-  async function handleSubmit() {
-    console.log('submit');
-    const response = await fetch('data.json');
-    const data = await response.json();
-     console.log('submit');
-     setData(data.data[0].tag_categories[0]);
-     return data;
-   }
+  // async function handleSubmit() {
+  //   console.log('submit');
+  //   const response = await fetch('data.json');
+  //   const data = await response.json();
+  //    console.log('submit');
+  //    setData(data.data[0].tag_categories[0]);
+  //    return data;
+  //  }
+
   return (
     <body className='container'>
       <h1 className='sheSharp'>
         SheSharp React.js - Check out this JSON data!
+        Hi this is Alex
+        testing
+        <br/> hello
       </h1>
       <p className='styledData'>
         {data}  
       </p>
-      <Button text='Click me!' handleClick={()=>handleSubmit()}/>      
+      {/* <Button text='Click me!' handleClick={()=>handleSubmit()}/>       */}
     </body>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home (){
+
+    return (
+        <h1>Welcome</h1>
+    );
+}
+
+export default Home;
+
+
+
+
