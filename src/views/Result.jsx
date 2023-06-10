cc9d043af6e6e657c3b1528148f0ff9d21978be6
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-06-10 11:52:45 -0400

    individual card flipping

--- src/views/Result.jsx
+++ src/views/Result.jsx
@@ -3,22 +3,25 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { Card, Button, Container } from "react-bootstrap";
 import "../styles/Result.css"; // Import the CSS file
 
-export default function Result({mood="sad"}) {
+export default function Result({ mood = "sad" }) {
   const navigate = useNavigate();
   const location = useLocation();
-  const [isCardFlipped, setIsCardFlipped] = useState(false);
 
   const { jobData } = location.state;
 
-  let task = '';
+  const [flippedCards, setFlippedCards] = useState([]);
 
-  console.log("jobData in Result", jobData);
-
-  const handleCardClick = () => {
-    setIsCardFlipped(!isCardFlipped);
+  const handleCardClick = (index) => {
+    setFlippedCards((prevFlippedCards) => {
+      const newFlippedCards = [...prevFlippedCards];
+      newFlippedCards[index] = !newFlippedCards[index];
+      return newFlippedCards;
+    });
   };
 
-  if(mood === "sad"){
+  let task = '';
+
+  if (mood === "sad") {
     task = "Research this company and see if your values are aligned.";
   } else if (mood === "neutral") {
     task = "Send an application to this company";
@@ -28,28 +31,35 @@ export default function Result({mood="sad"}) {
 
   return (
     <div className="d-flex text-center vh-100">
-      <Container> 
-        <Card
-          className={`job-card text-center mx-auto ${isCardFlipped ? "flipped" : ""}`}
-          onClick={handleCardClick}
-        >
-          <div className="card-inner">
-            <div className="card-front">
-              <Card.Body>
-                <Card.Title>You can do this!</Card.Title>
-                <Card.Text>Task: {task}</Card.Text>
-              </Card.Body>
+      <Container>
+        {jobData.map(({ job_name, company_name, post_url }, index) => (
+          <Card
+            key={index}
+            className={`job-card text-center mx-auto ${
+              flippedCards[index] ? "flipped" : ""
+            }`}
+            onClick={() => handleCardClick(index)}
+          >
+            <div className="card-inner">
+              <div className="card-front">
+                <Card.Body>
+                  <Card.Title>You can do this!</Card.Title>
+                  <Card.Text>Task: {task}</Card.Text>
+                </Card.Body>
+              </div>
+              <div className="card-back">
+                <Card.Body>
+                  <Card.Title>{job_name}</Card.Title>
+                  <Card.Text>{company_name}</Card.Text>
+                  <a href={post_url} target="_blank" rel="noopener noreferrer">
+                    link
+                  </a>
+                </Card.Body>
+              </div>
             </div>
-            <div className="card-back">
-              <Card.Body>
-                <Card.Title>{jobData[0].job_name}</Card.Title>
-                <Card.Text>{jobData[0].company_name}</Card.Text>
-                <a href={jobData[0].post_url} target="_blank" >link</a>
-              </Card.Body>
-            </div>
-          </div>
-        </Card>
-        
+          </Card>
+        ))}
+
         <div className="mt-3 text-center">
           <Button variant="primary" onClick={() => navigate("/")}>
             Go Back
