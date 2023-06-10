d59a7a4891794d730160986401e6194fc7148e93
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-10 11:33:09 -0500

    Hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './views/Home';
 import Result from './views/Result';
 import jobs from './db/jobs.json';
@@ -8,18 +8,18 @@ import { useEffect, useState } from 'react';
 // import AppJudith from '../AppJudithJudith';
 
 export default function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
-      path: "/",
+      path: '/',
       element: <Home />,
     },
     {
-      path: "/daily-challenge",
-      element: <Result/>,
+      path: '/daily-challenge',
+      element: <Result />,
     },
   ]);
 
   // const [data, setData] = useState('initial data');
 
-  return (<RouterProvider router={router}/>);
+  return <RouterProvider router={router} />;
 }
